75e3385cdbe0186f65a883a9d4ea0f74cdebb976
Nicole D <nnardella@gmail.com>
2018-10-26 16:15:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each { |call| call.length }
-    if call >= 4
+  
+    if array.length >= 4
       return true
     else
       return false
