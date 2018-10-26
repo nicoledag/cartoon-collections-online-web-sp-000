9d84553f7b1c4f648752829ce096067f7addf83b
Nicole D <nnardella@gmail.com>
2018-10-26 16:28:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,5 +30,10 @@ def find_the_cheese(array)
 
    array.each {|cheese_type| cheese_type.include?(cheese_types)}
 
-  
+   if array.include?(cheese_type)
+     return array
+   else
+     return nil
+   end
+
 end
