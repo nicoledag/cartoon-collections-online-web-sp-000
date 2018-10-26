ec30ce13796382d2c091137af9ecd7fc1129c648
Nicole D <nnardella@gmail.com>
2018-10-26 15:48:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+
+binding.pry
 end
 
 def summon_captain_planet# code an argument here
