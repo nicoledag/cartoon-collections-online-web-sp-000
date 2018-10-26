1bc8997d75f1fa1eaee9370a2f9d9f6dbb904122
Nicole D <nnardella@gmail.com>
2018-10-26 15:56:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ require 'pry'
 def roll_call_dwarves(array)# code an argument here
 
   ordered_name = []
-    array.each_with_index |name,i|
+    array.each_with_index do |name,i|
      ordered_name << "#{i+1}. #{name}"
     end
     puts ordered_name
